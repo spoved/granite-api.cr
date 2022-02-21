611cac8dd2d4b421b45f7df561ecbddc78d88289
Holden Omans <holden.omans@gmail.com>
2022-02-21 15:08:45 -0500

    minor update

--- src/ext/granite.cr
+++ src/ext/granite.cr
@@ -9,9 +9,13 @@ module Granite::Type
 end
 
 class Granite::Base
-  macro enum_column(name, _enum, _default)
-    column {{name.id}} : {{_enum}} = {{_enum}}::{{_default}},
-      converter: Granite::Converters::Enum({{_enum}}, String)
+  macro enum_column(name, _enum, _default = nil)
+    {% typ = _enum.resolve %}
+    {% if typ.union? && typ.nilable? %}
+    {% typ = typ.union_types.reject { |t| t == Nil }.first %}
+    {% end %}
+    column {{name.id}} : {{_enum}} {% if _default %} = {{_enum}}::{{_default}} {% end %},
+      converter: Granite::Converters::Enum({{typ}}, String)
   end
 
   macro unix_timestamps
