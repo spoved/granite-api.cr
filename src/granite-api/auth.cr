c829f71bbeba161f201083af8298c2c5f58036ac
Holden Omans <holden.omans@gmail.com>
2022-08-04 17:03:02 -0400

    remove cache headers from error and unauthed responses

--- src/granite-api/auth.cr
+++ src/granite-api/auth.cr
@@ -16,6 +16,8 @@ module Granite::Api
       def authorized?(env, security : Array(Open::Api::Security::Requirement)? = nil)
         return true if ENV["KEMAL_ENV"]? == "test" || ENV["KEMAL_ENV"]? == "development"
 
+        env.response.headers.delete("Cache-Control")
+
         token = env.get?("token")
         if token.is_a?(BearerToken)
           raise Unauthorized.new(token.error || "unauthorized") unless token.valid?
@@ -31,7 +33,7 @@ module Granite::Api
 
       def unauthorized_resp(env, msg)
         env.response.status_code = 403
-        env.response.print ({code: 403, message: msg}.to_json)
+        env.response.print({code: 403, message: msg}.to_json)
         env.response.close
       end
     end
--- src/granite-api/helpers/response.cr
+++ src/granite-api/helpers/response.cr
@@ -1,5 +1,6 @@
 module Granite::Api
   def not_found_resp(env, msg)
+    env.response.headers.delete("Cache-Control")
     env.response.status_code = 404
     env.response.content_type = "application/json"
     env.response.print(set_content_length({code: 404, message: msg}.to_json, env))
@@ -12,6 +13,7 @@ module Granite::Api
   end
 
   macro resp_400(env, msg)
+    {{env}}.response.headers.delete("Cache-Control")
     {{env}}.response.status_code = 400
     {{env}}.response.content_type = "application/json"
     halt {{env}}, status_code: 400, response: ({code: 400, message: {{msg}}}.to_json)
